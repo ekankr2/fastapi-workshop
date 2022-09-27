d34e50d11f001b7ed07c13952b27410686e11dce
ekankr2 <ekankr2@naver.com>
2022-09-27 17:46:11 +0900

    dynamic routes

--- fastapi-basic/main.py
+++ fastapi-basic/main.py
@@ -8,6 +8,16 @@ def index():
     return {'data': {'name': 'Sarthak'}}
 
 
-@app.get('/about')
-def about():
-    return {'data': 'about page'}
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data': 'all unpublished blogs'}
+
+
+@app.get('/blog/{id}')
+def show(id: int):
+    return {'data': id}
+
+
+@app.get('/blog/{id}/comments')
+def comments(id):
+    return {'data': {'1', '2'}}
