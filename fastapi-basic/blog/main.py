336f17d2944b14ae4324cfdd2bd71ec3f137d0c4
ekankr2 <ekankr2@naver.com>
2022-09-27 18:51:49 +0900

    create row post

--- fastapi-basic/blog/main.py
+++ fastapi-basic/blog/main.py
@@ -1,13 +1,27 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+
 from . import schemas, models
 from blog.schemas import Blog
-from .database import engine
+from .database import engine, SessionLocal
 
 app = FastAPI()
 
 models.Base.metadata.create_all(engine)
 
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
 @app.post('/blog')
-def create(request: Blog):
-    return request
+def create(request: Blog, db: Session = Depends(get_db)):
+    new_blog = models.Blog(title=request.title, body=request.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
