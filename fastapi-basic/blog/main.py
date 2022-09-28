251cbf7338e034fdeeb754ca0cc8475bab27f8af
ekankr2 <ekankr2@naver.com>
2022-09-28 18:03:57 +0900

    response model

--- fastapi-basic/blog/main.py
+++ fastapi-basic/blog/main.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from fastapi import FastAPI, Depends, status, Response, HTTPException
 from sqlalchemy.orm import Session
 
@@ -27,7 +29,7 @@ def create(request: Blog, db: Session = Depends(get_db)):
     return new_blog
 
 
-@app.get('/blog')
+@app.get('/blog', response_model=List[schemas.ShowBlog])
 def get_blogs(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
@@ -55,7 +57,7 @@ def update_blog(id, request: schemas.Blog, db: Session = Depends(get_db)):
     return 'updated'
 
 
-@app.get('/blog/{id}', status_code=200)
+@app.get('/blog/{id}', status_code=200, response_model=schemas.ShowBlog)
 def show(id, response: Response, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
     if not blog:
