22f6342fd5ef0c7cc826b589a74b4c46b4bffc27
ekankr2 <ekankr2@naver.com>
2022-09-28 18:34:19 +0900

    hashing password

--- fastapi-basic/blog/main.py
+++ fastapi-basic/blog/main.py
@@ -1,11 +1,10 @@
 from typing import List
-
 from fastapi import FastAPI, Depends, status, Response, HTTPException
 from sqlalchemy.orm import Session
-
 from blog.schemas import Blog
 from . import models, schemas
 from .database import engine, SessionLocal
+from .hashing import Hash
 
 app = FastAPI()
 
@@ -70,7 +69,7 @@ def show(id, response: Response, db: Session = Depends(get_db)):
 
 @app.post('/user')
 def create_user(request: schemas.User, db: Session = Depends(get_db)):
-    new_user = models.User(name=request.name, email=request.email, password=request.password)
+    new_user = models.User(name=request.name, email=request.email, password=Hash.bcrypt(request.password))
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
