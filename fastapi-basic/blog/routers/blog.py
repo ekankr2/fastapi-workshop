377ec393b8eaac82c660d8cc55cb19cc50730bb6
ekankr2 <ekankr2@naver.com>
2022-09-30 18:13:03 +0900

    login authentication

--- fastapi-basic/blog/routers/blog.py
+++ fastapi-basic/blog/routers/blog.py
@@ -1,9 +1,7 @@
 from typing import List
-
 from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
-
-from .. import schemas
+from .. import schemas, oauth2
 from ..database import get_db
 from ..repository import blog
 
@@ -14,7 +12,7 @@ router = APIRouter(
 
 
 @router.get('', response_model=List[schemas.ShowBlog])
-def get_blogs(db: Session = Depends(get_db)):
+def get_blogs(db: Session = Depends(get_db), current_user: schemas.User = Depends(oauth2.get_current_user)):
     return blog.get_all(db)
 
 
