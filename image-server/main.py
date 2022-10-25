43c12cb62e3f68fb844e436c1c3859566107d01a
ekankr2 <ekankr2@naver.com>
2022-10-25 16:11:55 +0900

    saving uploaded file in different location

--- image-server/main.py
+++ image-server/main.py
@@ -1,14 +1,28 @@
 import shutil
 from typing import List
-
 from fastapi import FastAPI, UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*']
+)
 
 @app.post("/")
 async def root(file: UploadFile = File(...)):
-    with open('test.mp4', "wb") as buffer:
+    file_location = f"files/{file.filename}"
+    with open(file_location, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
 
     return {"file_name": file.filename}
