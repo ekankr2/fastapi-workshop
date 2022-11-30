544c930d713deebbfca96bf34801eafe1836845f
ekankr2 <ekankr2@naver.com>
2022-11-30 11:09:30 +0900

    test: test_create_user

--- fastapi-basic/blog/tests/crud/test_user.py
+++ fastapi-basic/blog/tests/crud/test_user.py
@@ -1,5 +1,6 @@
 from sqlalchemy.orm import Session
 
+from blog.hashing import Hash
 from blog.repository import user
 from blog.schemas import User
 from blog.tests.utils.utils import random_email, random_lower_string
@@ -8,7 +9,8 @@ from blog.tests.utils.utils import random_email, random_lower_string
 def test_create_user(db: Session) -> None:
     email = random_email()
     password = random_lower_string()
-    user_in = User(email=email, password=password)
-    new_user = user.create()
+    name = 'ekan'
+    user_in = User(email=email, password=Hash.bcrypt(password), name=name)
+    new_user = user.create(user_in, db)
     assert new_user.email == email
-    assert hasattr(new_user, "hashed_password")
+    assert new_user.name == name
